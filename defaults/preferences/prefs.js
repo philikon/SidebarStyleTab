62b8d8c4eb85b40dee50b892567171d0787c9d2f
Philipp von Weitershausen <philipp@weitershausen.de>
2010-01-09 22:58:22 +0000

    get rid of unnecessary stuff

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,2 @@
-pref("extensions.SidebarStyleTabs.boolpref", false);
-pref("extensions.SidebarStyleTabs.intpref", 0);
-pref("extensions.SidebarStyleTabs.stringpref", "A string");
-
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.sidebarstyletabs@philikon.de.description", "chrome://SidebarStyleTabs/locale/overlay.properties");
