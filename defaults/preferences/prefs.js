3575fe43140361901d8f107567c252fdd10b692c
Philipp von Weitershausen <philipp@weitershausen.de>
2010-01-11 14:48:37 +0000

    lowercase package name

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,2 +1,2 @@
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.sidebarstyletab@philikon.de.description", "chrome://SidebarStyleTab/locale/overlay.properties");
+pref("extensions.sidebarstyletab@philikon.de.description", "chrome://sidebarstyeltab/locale/overlay.properties");
